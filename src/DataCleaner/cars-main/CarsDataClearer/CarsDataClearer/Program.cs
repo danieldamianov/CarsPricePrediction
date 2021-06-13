1d616b1429e2da38add17778b362c1470253521b
Daniel Damianov <56774476+danieldamianov@users.noreply.github.com>
2021-06-13 23:30:59 +0300

    Added the data cleaner project

---
+++ src/DataCleaner/cars-main/CarsDataClearer/CarsDataClearer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CarsDataClearer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carsInfo = File.ReadAllLines("data.csv");
+
+            foreach (var item in carsInfo)
+            {
+                var carsInfoSplit = item.Split(",");
+                bool isValid = true;
+                for (int i = 8; i <= 24; i++)
+                {
+                    if (carsInfoSplit[i] == "-1")
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (isValid)
+                {
+                    File.AppendAllText("cleanData.csv", item + Environment.NewLine, Encoding.UTF8);
+                }
+            }
+            
+        }
+    }
+}
